b0b2b382c3806a3dc4d332f480b69733c8c2f726
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-03-22 19:24:48 +0600

    update

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <ChatSidebar/>
+            {/* <ChatSidebar/> */}
           </div>
             <ChatInbox/>
         </div>
